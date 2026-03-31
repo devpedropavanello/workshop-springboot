cbd784ecf242eb75956dce73597c54b980ff2aa8
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-03-31 16:02:10 -0300

    feat: implementa associação OneToMany entre Product e OrderItem

--- src/main/java/io/github/devpedropavanello/workshop_springboot/entities/Product.java
+++ src/main/java/io/github/devpedropavanello/workshop_springboot/entities/Product.java
@@ -23,10 +23,14 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
+        super();
         this.id = id;
         this.name = name;
         this.description = description;
@@ -76,6 +80,14 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
