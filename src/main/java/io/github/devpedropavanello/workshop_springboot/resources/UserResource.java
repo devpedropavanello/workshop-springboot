cde6f58d7ad886124e97f82ea90f0d67e0021e28
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-04-02 11:18:58 -0300

    feat: implementa exclusão de usuário

--- src/main/java/io/github/devpedropavanello/workshop_springboot/resources/UserResource.java
+++ src/main/java/io/github/devpedropavanello/workshop_springboot/resources/UserResource.java
@@ -29,10 +29,17 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = service.insert(obj);
         URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
